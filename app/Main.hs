a700d19000ae1bf6b591f8d8078d4b0059ae6e27
Erik Göök <erikgook@gmail.com>
2018-10-30 22:05:05 +0100

    Sketches out routes to the api
    Using scotty.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Web.Scotty
+import Data.Monoid (mconcat)
 
 main :: IO ()
-main = someFunc
+main = scotty 3010 $ do
+    get "/hosts" $ html "<p>GET /hosts </p>"
+    get "/hosts/:hostId/appointments" $ do
+        hostId <- param "hostId"
+        html $ mconcat ["<p>GET /host/", hostId, "/appointments </p>"]
+    post "/bookings/:appointmentId" $ do
+        apptId <- param "appointmentId"
+        html $ mconcat ["<p>POST /bookings/", apptId, "</p>"]
+    get "/bookings" $ html "<p>GET /bookings</p>"
+
